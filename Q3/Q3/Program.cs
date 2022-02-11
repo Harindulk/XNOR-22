7d25afc15d1378b607892bd99cbd95ae9c96fe87
harindulk <harindulakshitha525@gmail.com>
2022-02-11 20:56:39 +0530

    update

---
+++ Q3/Q3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string s, revs = "";
+        s = Console.ReadLine();
+
+
+        for (int i = s.Length - 1; i >= 0; i--) //String Reverse  
+        {
+            revs += s[i].ToString();
+        }
+        if (revs == s) // Checking whether string is palindrome or not  
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
