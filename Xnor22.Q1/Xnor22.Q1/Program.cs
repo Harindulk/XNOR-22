e8c0804f83591f745b8a62577e0793c6abe9dd8d
harindulk <harindulakshitha525@gmail.com>
2022-02-11 20:49:17 +0530

    upload solution 01

---
+++ Xnor22.Q1/Xnor22.Q1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int N = int.Parse(Console.ReadLine());
+        for (int i = 0; i < N; i++)
+            Console.WriteLine(new String('*', i + 1).PadRight(N, ' '));
+
+    }
+}
