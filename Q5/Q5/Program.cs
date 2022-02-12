fb23ff1981f1d844bbfd583b02c4552ebed891d5
harindulk <harindulakshitha525@gmail.com>
2022-02-12 22:42:43 +0530

    update

---
+++ Q5/Q5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
